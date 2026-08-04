eaf63c2235a08efe14eca31170bdecb202bf3e35
syakirscm <syakir.scm@gmail.com>
2026-08-04 20:26:06 +0700

    Update syncConfig.ts

--- src/config/syncConfig.ts
+++ src/config/syncConfig.ts
@@ -6,5 +6,4 @@
 
 export const DEFAULT_GAS_URL =
   (typeof import.meta !== 'undefined' && (import.meta as any).env?.VITE_GAS_URL) ||
-  'https://script.google.com/macros/s/AKfycbx_EXAMPLE_REPLACE_WITH_YOUR_DEPLOYED_EXEC_URL/exec';
-"https://script.google.com/macros/s/AKfycbzSdMuaCID-g6uzlaV7ydY8Ec6jp4FgYcB-XCIRC5qzkJxpXgeyjtAPAHqsGXODUXrz/exec";
+  'https://script.google.com/macros/s/AKfycbzSdMuaCID-g6uzlaV7ydY8Ec6jp4FgYcB-XCIRC5qzkJxpXgeyjtAPAHqsGXODUXrz/exec';
