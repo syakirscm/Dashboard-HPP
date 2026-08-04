49c37ed2fd747e72b0fd4f2a4e360120f6516083
syakirscm <syakir.scm@gmail.com>
2026-08-04 19:26:38 +0700

    Delete src/src/config directory

--- src/src/config/syncConfig.ts
+++
@@ -1,3 +0,0 @@
-export const DEFAULT_GAS_URL =
-  (typeof import.meta !== 'undefined' && (import.meta as any).env?.VITE_GAS_URL) ||
-  'https://script.google.com/macros/s/AKfycbx_GANTI_DENGAN_URL_DEPLOY_APPS_SCRIPT_ANDA/exec';
